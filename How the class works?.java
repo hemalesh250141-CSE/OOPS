c9a54babcf65ba96e8ee1eb46377d88488fe497c
hemalesh250141-CSE <hemalesh.250141@cse.ritchennai.edu.in>
2026-09-14 13:28:19 +0530

    Refactor Student class documentation and comments
    Removed comments and unnecessary lines to clean up the code.

---
+++ How the class works?.java
@@ -0,0 +1,45 @@
+//How it works
+class Student {
+
+//Creates a class called Student.
+//Inside it:
+
+String name;
+int age;
+
+//These are attributes/data members.
+
+//And:
+
+void display()
+
+//is a method.
+
+//Think of it like:
+
+Student
+ ├── name
+ ├── age
+ └── display()
+Real-time example
+
+//In a college application, you might have:
+
+class Student {
+    String name;
+    int rollNo;
+    String department;
+}
+
+//This class defines what information every student should have.
+
+//Where classes are used
+
+//Almost everywhere in large Java applications: 
+//Banking systems
+//College management
+//E-commerce
+//Hospital management
+//Games
+//Android applications
+//Employee management
