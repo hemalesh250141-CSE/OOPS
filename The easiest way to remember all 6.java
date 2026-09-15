703981cd345ea813d1b6d8e79f36450ea59d08d3
hemalesh250141-CSE <hemalesh.250141@cse.ritchennai.edu.in>
2026-09-15 18:46:57 +0530

    Create The easiest way to remember all 6.java

---
+++ The easiest way to remember all 6.java
@@ -0,0 +1,41 @@
+Think about a Car.
+
+//1. Class
+class Car
+
+Blueprint
+
+↓
+
+//2. Object
+Car c1 = new Car();
+
+Actual car
+
+↓
+
+///3. Constructor
+Car(String model)
+
+Initializes the car
+
+↓
+
+//4. Encapsulation
+private String model;
+
+Protects the data
+
+↓
+
+//5. Inheritance
+class ElectricCar extends Car
+
+ElectricCar gets features from Car
+
+↓
+
+//6. Polymorphism
+void start()
+
+Different types of cars can implement start() differently.
