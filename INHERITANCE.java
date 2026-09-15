c7cc3f80944d18814628d2345de03fb6218cab75
hemalesh250141-CSE <hemalesh.250141@cse.ritchennai.edu.in>
2026-09-15 18:41:54 +0530

    Create INHERITANCE.java

---
+++ INHERITANCE.java
@@ -0,0 +1,23 @@
+class Vehicle {
+
+    void start() {
+        System.out.println("Vehicle started");
+    }
+}
+
+class Car extends Vehicle {
+
+    void drive() {
+        System.out.println("Car is driving");
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+
+        Car c = new Car();
+
+        c.start();
+        c.drive();
+    }
+}
